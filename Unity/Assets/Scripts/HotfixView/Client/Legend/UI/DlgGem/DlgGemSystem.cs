a362fda6baf76313471b4ab4201a34370e97f6b5
DESKTOP-QQOLK7G\LDYQ <394266315@qq.com>
2026-03-12 20:48:17 +0800

    宝石界面

--- Unity/Assets/Scripts/HotfixView/Client/Legend/UI/DlgGem/DlgGemSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Legend/UI/DlgGem/DlgGemSystem.cs
@@ -12,7 +12,24 @@ namespace ET.Client
 
 		public static void RegisterUIEvent(this DlgGem self)
 		{
-		 
+			self.View.E_CloseButtonButton.AddListener(self.OnCloseButton);
+			self.View.E_GemCombingBtnButton.AddListener(self.OnGemCombingButton);
+			self.View.E_GemInlayBtnButton.AddListener(self.OnGGemInlayButton);
+		}
+
+		private static void OnGemCombingButton(this DlgGem self)
+		{
+			
+		}
+
+		private static void OnGGemInlayButton(this DlgGem self)
+		{
+			
+		}
+
+		private static void OnCloseButton(this DlgGem self)
+		{
+			self.Root().GetComponent<UIComponent>().CloseWindow(WindowID.WindowID_Gem);
 		}
 
 		public static void ShowWindow(this DlgGem self, Entity contextData = null)
