c2804a60283fcae3e520d62da3976610db02ffee
DESKTOP-QQOLK7G\LDYQ <394266315@qq.com>
2026-03-14 18:31:17 +0800

    角色属性界面

---
+++ Unity/Assets/Scripts/HotfixView/Client/Legend/UIBehaviour/CommonUI/ES_RoleAttributeViewSystem.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[EntitySystemOf(typeof(ES_RoleAttribute))]
+	[FriendOfAttribute(typeof(ES_RoleAttribute))]
+	public static partial class ES_RoleAttributeSystem 
+	{
+		[EntitySystem]
+		private static void Awake(this ES_RoleAttribute self,Transform transform)
+		{
+			self.uiTransform = transform;
+		}
+
+		[EntitySystem]
+		private static void Destroy(this ES_RoleAttribute self)
+		{
+			self.DestroyWidget();
+		}
+	}
+
+
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/Legend/UIBehaviour/CommonUI/ES_RoleAttribute.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+namespace ET.Client
+{
+	[ChildOf]
+	[EnableMethod]
+	public  class ES_RoleAttribute : Entity,ET.IAwake<UnityEngine.Transform>,IDestroy 
+	{
+		    public Transform UITransform
+         {
+     	    get
+     	    {
+     		    return this.uiTransform;
+     	    }
+     	    set
+     	    {
+     		    this.uiTransform = value;
+     	    }
+         }
+
+		public void DestroyWidget()
+		{
+			this.uiTransform = null;
+		}
+
+		public Transform uiTransform = null;
+	}
+}
